877b36af27f328e74e23920bd5c07aa1c1222ceb
marinjorian <jixiao@mail.med.upenn.edu>
2015-11-20 12:55:51 -0500

    code for Figure 5ab

--- plotBarAndPiePlot_fig5ab.r
+++ plotBarAndPiePlot_fig5ab.r
@@ -1,12 +1,12 @@
 library(ggplot2)
 
-#Figure 5. Barplot:proportionOfHGMD
+##Figure 5. Barplot:proportionOfHGMD
 fracDat=data.frame(frac=rep(NA,3),geneSet=rep("",3))
 fracDat$frac=c(1235/3229,1167/4757,3302/19568)
 fracDat$geneSet=c("EG\n1235/3229","NEG\n1167/4757","All\n3302/19568")
 fracDat$geneSet=factor(fracDat$geneSet,levels=c("EG\n1235/3229","NEG\n1167/4757","All\n3302/19568"))
 
-png("/home/jixiao/pgfi_data/ifs/jixiao/programs/generalData/geneList/IMPC_paper_freeze/freeze_102315/figure5a_proportionOfHGMD.png",height=3,width=6,units='in',res=500)
+png("~/figure5a_proportionOfHGMD.png",height=3,width=6,units='in',res=500)
 ggplot(fracDat,aes(x=geneSet,y=frac,fill=geneSet)) +
 	scale_fill_discrete(guide = FALSE) +
 	geom_bar(stat="identity") +
@@ -14,13 +14,15 @@ ggplot(fracDat,aes(x=geneSet,y=frac,fill=geneSet)) +
 	theme_bw() + xlab("Gene set") + ylab("Proportion of HGMD(DM) genes") + ylim(c(0,0.5))
 dev.off()
 
-#Figure 5b. Piechart:HGMDfraction
+
+
+##Figure 5b. Piechart:HGMDfraction
 fracDat=data.frame(frac=rep(NA,3),geneSet=rep("",3))
 fracDat$frac=c(1235/3302,1167/3302,1-(1235+1167)/3302)
 fracDat$geneSet=c("EG: 1235 (37.40%)","NEG: 1167 (35.34%)","Unknown: 900 (27.26%)")
 fracDat$geneSet=factor(fracDat$geneSet,levels=c("EG: 1235 (37.40%)","NEG: 1167 (35.34%)","Unknown: 900 (27.26%)"))
 
-png("/home/jixiao/pgfi_data/ifs/jixiao/programs/generalData/geneList/IMPC_paper_freeze/freeze_102315/figure5b_HGMDfraction.png",height=3,width=6,units='in',res=500)
+png("~/figure5b_HGMDfraction.png",height=3,width=6,units='in',res=500)
 ggplot(fracDat,aes(x="",y=frac,fill=geneSet)) +
 	scale_fill_discrete(name="Gene set") +
 	geom_bar(stat="identity") +
